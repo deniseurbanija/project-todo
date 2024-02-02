f7136113d7e2e214229bb9b211552d014046f9ab
Denise <deniseurbanija1@gmail.com>
2024-02-02 19:10:16 -0300

    components done

--- client/App.js
+++ client/App.js
@@ -1,48 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import Task from './components/Task';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import InputTask from "./components/InputTask";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    fecthData();
-  }, [])
+    fecthTodos();
+  }, []);
 
-  async function fecthData() {
-    const response = await fetch("http://192.168.1.86:8080/todos/1")
+  async function fecthTodos() {
+    const response = await fetch("http://192.168.1.86:8080/todos/1");
     const data = await response.json();
     setTodos(data);
   }
 
+  function clearTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) =>
+        todo.id == id
+          ? { ...todo, completed: todo.completed === 1 ? 0 : 1 }
+          : todo
+      )
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <SafeAreaView>
-        <FlatList
-        data = {todos}
-        keyExtractor={(todo) => todo.id}
-        renderItem={({item}) => <Task {...item}/>}
-        ListHeaderComponent={() => <Text style={styles.title}>Today</Text>}
-        contentContainerStyle={styles.contentContainerStyle}
-        />
-      </SafeAreaView>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView flex={1}>
+      <BottomSheetModalProvider>
+        <StatusBar />
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            data={todos}
+            contentContainerStyle={styles.contentContainerStyle}
+            keyExtractor={(todo) => todo.id}
+            renderItem={({ item }) => (
+              <Task {...item} toggleTodo={toggleTodo} clearTodo={clearTodo} />
+            )}
+            ListHeaderComponent={() => <Text style={styles.title}>Today</Text>}
+          />
+          <InputTask todos={todos} setTodos={setTodos} />
+        </SafeAreaView>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f9fbf2',
+    backgroundColor: "#f9fbf2",
   },
   contentContainerStyle: {
-    padding: 15
+    padding: 15,
   },
   title: {
     fontWeight: "800",
     fontSize: 28,
-    marginBottom: 15
+    marginBottom: 15,
   },
 });
--- client/components/Task.js
+++ client/components/Task.js
@@ -1,3 +1,4 @@
+import * as React from "react";
 import {
   View,
   Text,
@@ -6,25 +7,121 @@ import {
   Pressable,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import { useRef, useState } from "react";
+import "react-native-gesture-handler";
+import SharedTodoModalContent from "./SharedTodoModalContent";
+import TodoModalContent from "./TodoModalContent";
 
 function CheckMark({ id, completed, toggleTodo }) {
+  async function toggle() {
+    const response = await fetch(`http://192.168.1.86:8080/todos/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PUT",
+      body: JSON.stringify({
+        value: completed ? false : true,
+      }),
+    });
+    const data = await response.json();
+    toggleTodo(id);
+    console.log(data);
+  }
   return (
     <Pressable
+      onPress={toggle}
       style={[
         styles.checkMark,
-        { backgroundColor: completed === 0 ? "#86ba90" : "#E9E9EF" },
+        { backgroundColor: completed === 0 ? "#E9E9EF" : "#86ba90" },
       ]}
     ></Pressable>
   );
 }
 
-export default function Task({ id, title, shared_with_id, completed }) {
+export default function Task({
+  id,
+  title,
+  shared_with_id,
+  completed,
+  clearTodo,
+  toggleTodo,
+}) {
+  const [isDeleteActive, setIsDeleteActive] = useState(false);
+  const bottomSheetModalRef = useRef(null);
+  const sharedBottomSheetRef = useRef(null);
+  const snapPoints = ["25%", "48%", "75%"];
+  const snapPointsShared = ["40%"];
+
+  function handlePresentModal() {
+    bottomSheetModalRef.current?.present();
+  }
+
+  function handlePresentShared() {
+    sharedBottomSheetRef.current?.present();
+  }
+
+  function deleteTodo() {
+    const response = fetch(`http://192.168.1.86:8080/todos/${id}`, {
+      method: "DELETE",
+    });
+    clearTodo(id);
+    console.log(response.status);
+  }
+
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      onLongPress={() => setIsDeleteActive(true)}
+      onPress={() => setIsDeleteActive(false)}
+      activeOpacity={0.8}
+      style={[styles.container]}
+    >
       <View style={styles.containerTextCheckBox}>
-        <CheckMark/>
+        <CheckMark id={id} completed={completed} toggleTodo={toggleTodo} />
         <Text style={styles.text}>{title}</Text>
       </View>
+      {shared_with_id !== null ? (
+        <Feather
+          onPress={handlePresentShared}
+          name="users"
+          size={20}
+          color="#383839"
+        />
+      ) : (
+        <Feather
+          onPress={handlePresentModal}
+          name="share"
+          size={20}
+          color="#383839"
+        />
+      )}
+      {isDeleteActive && (
+        <Pressable onPress={deleteTodo} style={styles.deleteButton}>
+          <Text style={{ color: "white", fontWeight: "bold" }}>x</Text>
+        </Pressable>
+      )}
+
+      <BottomSheetModal
+        ref={sharedBottomSheetRef}
+        snapPoints={snapPointsShared}
+        backgroundStyle={{ borderRadius: 50, borderWidth: 4 }}
+      >
+        <SharedTodoModalContent
+          id={id}
+          title={title}
+          shared_with_id={shared_with_id}
+          completed={completed}
+        />
+      </BottomSheetModal>
+
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={2}
+        snapPoints={snapPoints}
+        backgroundStyle={{ borderRadius: 50, borderWidth: 4 }}
+      >
+        <TodoModalContent id={id} title={title} />
+      </BottomSheetModal>
     </TouchableOpacity>
   );
 }
